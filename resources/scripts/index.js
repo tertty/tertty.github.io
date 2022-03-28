a5d9f4acb3bb10fa50f55c57f5154d21ae45e022
Taylor E <taylore@dataflowsys.com>
2022-03-28 16:45:52 -0400

    Card animations and take center stage

--- resources/scripts/index.js
+++ resources/scripts/index.js
@@ -1,3 +1,13 @@
-function enlarge_business_card() {
-    document.getElementById('peek_business_card').id='main_business_card'; console.log('YO');
+const landing_card = document.getElementById("landing_card");
+landing_card.addEventListener("animationed", landing_card_end_listener, false);
+landing_card.addEventListener("click", landing_card_end_listener, false);
+
+function landing_card_end_listener(event) {
+    switch(event.type) {
+        case "click":
+            document.getElementById("landing_card_hint_text").className = "center_stage_hint_text_transition";
+            document.getElementById("peek_business_card").className = "grow_business_card_transition";
+            landing_card.className = "center_stage_business_card_transition";
+            break;
+    }
 }
